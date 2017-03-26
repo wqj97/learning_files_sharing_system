2902d3638ff8f214391c062355ec45f0bfb3f04b
broven <zycode277@163.com>
2017-03-26 09:58:59 +0800

    添加fileIcon组件

---
+++ front-end/src/components/fileIcon.vue
@@ -0,0 +1,43 @@
+<template>
+
+  <img :src="fileUrl" :alt="type" :class="size ? size : ''">
+
+</template>
+
+<script>
+const imgBaseUrl = '/static/img/fileType/'
+export default {
+  name: 'fileIcon',
+  props: {
+    type: {
+      type: String,
+      require: true
+    },
+    size: {
+      type: String,   // big small
+      require: false,
+      default: 'big'
+    }
+  },
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+    fileUrl() {
+      return imgBaseUrl + this.type.toLowerCase() + '.png'
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.big{
+  width: 100px;
+  height: 115px;
+}
+.small {
+  width: 57px;
+  height: 66px;
+}
+</style>
