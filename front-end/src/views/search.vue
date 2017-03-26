b3c69ea50ffa9abd54c32d21fbff4e90b036b74e
broven <zycode277@163.com>
2017-03-26 11:39:25 +0800

    搜索页面

--- front-end/src/views/search.vue
+++ front-end/src/views/search.vue
@@ -1,15 +1,37 @@
 <template>
   <div>
-
+  <searchBar :type="type"  shadow v-model="search"></searchBar>
+    {{search}}
+    <main class="container">
+    <fileList v-if="type"></fileList>
+    <categoryList v-else checkBox></categoryList>
+  </main>
   </div>
 </template>
 
 <script>
+import {searchBar, fileList, categoryList} from '@/components'
 export default {
-  name: '',
+  name: 'search',
+  components: {
+    searchBar, fileList, categoryList
+  },
+  mounted () {
+  	console.log(this.$route)
+    this.type = this.$route.query.type
+  },
+  data () {
+    return {
+      search: '',
+      type:''
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.container{
+  margin-top: 20px;
+}
 
 </style>
