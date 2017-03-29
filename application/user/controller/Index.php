0ecf02696e972479927fde860f041211a4834647
wqj97 <wqj97@126.com>
2017-03-29 17:57:39 +0800

    前端配置修改, 增加代理和路由转换

---
+++ application/user/controller/Index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wanqianjun
+ * Date: 2017/3/29
+ * Time: 下午5:49
+ */
+
+namespace app\user\controller;
+
+
+class Index
+{
+    public function Index() {
+
+    }
+}
--- front-end/config/index.js
+++ front-end/config/index.js
@@ -30,12 +30,20 @@ module.exports = {
     assetsPublicPath: '/',
     imgPublicPath: './dist/static/img',
     sourceImgPath: '/src/assets/',
-    proxyTable: {},
+    proxyTable: {
+      '/': {
+        target: 'http://wx.97qingnian.com',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/': '/'
+        }
+      }
+    },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
     // In our experience, they generally work as expected,
     // just be aware of this issue when enabling this option.
-    cssSourceMap: false
+    cssSourceMap: true
   }
 }
