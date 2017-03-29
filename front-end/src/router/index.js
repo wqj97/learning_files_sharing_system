916bf63e327f1977ef2e5880f4fd63b1355cc0ae
broven <zycode277@163.com>
2017-03-29 18:24:09 +0800

    PATCH router config

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,28 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-<<<<<<< HEAD
 
 const asyncRequire = viewName => resolve => require([`../views/${viewName}`], resolve)
-/**
- * 异步加载模块, 如果一些很少访问的views
- * 用 asyncRequire('XXX')就好
- * asyncRequire会自动加载views目录下指定的文件
- */
-=======
-const download = resolve => {
-  require.ensure([], (require) => {
-    resolve(require('v/download'))
-  })
-}
-const asyncImport = (component) =>
-   resolve => {
-    require.ensure([], (require) => {
-      // TODO: BUG: alias doesn't work
-      resolve(require(`../views/${component}`))
-    }, 'noneIndex')
-  }
-// import {asyncImport} from "@/utils"
->>>>>>> lazy loading router
 Vue.use(Router)
 const router = new Router({
   routes: [
