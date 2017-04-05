0d635dc9f96e4e1ba3584da4d324e0cdfc2c132d
broven <zycode277@163.com>
2017-04-05 15:15:04 +0800

    完善分类列表

--- front-end/src/components/categoryList.vue
+++ front-end/src/components/categoryList.vue
@@ -1,36 +1,64 @@
 <template>
   <div class="item_container">
-  <div v-for="item in renderList" class="item" @click="$emit('click', item)">
+  <div v-for="item in renderList" class="item" @click="click(item)">
      <div class="imgWarp">
         <img class="img" :src="item.image">
      </div>
       {{item.title}}
   <div class="checkbox" v-show="checkBox">
-    <img src="../assets/checked.svg"  alt="checked">
-    <img src="../assets/unChecked.svg" alt="unchecked">
+    <img v-if="item.checked" src="../assets/checked.svg"  alt="checked">
+    <img v-else src="../assets/unChecked.svg" alt="unchecked">
   </div>
   </div>
   </div>
 </template>
-
 <script>
+const ids =        [2        ,4        ,8      ,16    ,32       ,64      ,128    ]
 const titleList = ['科目习题', '科目精华', '英语', '考研', '资格考试', '工具书', '其他']
 const imgPath = '/static/img/category/'
 const defaultList = titleList.map((item, index) => {
   return {
     title: item,
-    image: `${imgPath}${index + 1}.png`
+    image: `${imgPath}${index + 1}.png`,
+    id: ids[index],
+    checked: false
   }
 })
 export default {
   props: {
+    value: {
+      type: Array
+    },
     checkBox: {
-      type: Boolean,
-      default: false
+      default: false,
+      type: Boolean
+    }
+  },
+  methods: {
+    click(item) {
+      this.$emit('click', item)
+      if (!this.checkBox) return
+      const index = this.select.indexOf(item)
+      if(index === -1) {
+        // 没有被选中
+        this.select.push(item)
+      } else {
+        // 被选中
+        this.select.splice(index, 1)
+      }
+      this.toggleCheck(item)
+      this.$emit('input', this.select)
+    },
+    toggleCheck(item) {
+      const index = this.renderList.indexOf(item)
+      const it  = this.renderList[index]
+      it.checked = !it.checked
+      this.$set(this.renderList, index, item)
     }
   },
   data () {
     return {
+      select: [],
       renderList : this.list ? this.list : defaultList
     }
   }
