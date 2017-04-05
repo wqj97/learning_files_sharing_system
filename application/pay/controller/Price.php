f74f691f9455053a3e502f026b8c970165a04f2a
wqj97 <wqj97@126.com>
2017-04-06 01:13:07 +0800

    后台更新

---
+++ application/pay/controller/Price.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace app\pay\controller;
+/**
+ * Created by PhpStorm.
+ * User: wanqianjun
+ * Date: 2017/4/6
+ * Time: 上午1:10
+ */
+class Price
+{
+    /**
+     * 获取价格列表
+     * @get
+     * @return \think\response\Json
+     */
+    public function Index()
+    {
+        $price = Server_Setting('price');
+        return json($price);
+    }
+}
