8fa217ceeda88749431eab41687db13bd8d10d6e
broven <zycode277@163.com>
2017-04-07 23:13:49 +0800

    首页轮播图修复

--- front-end/src/views/index.vue
+++ front-end/src/views/index.vue
@@ -1,62 +1,79 @@
 <template>
   <div>
-   <header id="header">
-      <div id="basic_info" class="text_shdow" >
-      <div class="left" @click="$router.push('/mine')">
-        <img class="avatar_photo vertical_center" :src="user['U_head']">
-      Lv.{{user.level}}
-      </div>
-      <div class="right" @click="$router.push('/schoolList')">
-        <img class="img vertical_center" src="../assets/location.png">
+    <header id="header">
+      <div id="basic_info"
+           class="text_shdow">
+        <div class="left"
+             @click="$router.push('/mine')">
+          <img class="avatar_photo vertical_center"
+               :src="user['U_head']"> Lv.{{user.level}}
+        </div>
+        <div class="right"
+             @click="$router.push('/schoolList')">
+          <img class="img vertical_center"
+               src="../assets/location.png">
           <span style="font-size:12px;">{{user['U_school']}}</span>
+        </div>
       </div>
-    </div>
-    <h3 class="title text_shdow">这里有你想要的所有</h3>
-    <div id="search" @click="$router.push('/search')">
-      <img src="../assets/search.png" class="img vertical_center">
-      <span >搜索...</span>
-    </div>
-    <div class="upload text_shdow" @click="$router.push('/upload')">
-      <img class="img vertical_center" src="../assets/upload.png">
-      <span>上传文件</span>
-    </div>
-   </header>
-   <section class="slide_show">
-    <swiper loop :list="imgList" auto :show-desc-mask="false" height="100px" style="width:100%;margin:0 auto; height:100%" dots-class="custom-bottom" dots-position="center"></swiper>
-   </section>
-   <section class="list">
-     <Tab v-model="tabIndex" :active-color="mainColor" :bar-active-color="mainColor" :line-width="2" defaultColor="#00B9F8">
-      <tab-item :selected="item === currentTab" v-for="(item, index) in tabList" :key="index" >{{item}}</tab-item>
-     </Tab>
-     <!--<swiper v-model="tabIndex" >
-        <swiper-item>
-         <categoryList></categoryList>
-        </swiper-item>
-         <swiper-item>
-         <categoryList></categoryList>
-        </swiper-item>
-      </swiper>-->
-<transition name="fade">
-  <categoryList v-if="tabIndex === 0" @click="categoryListClick"></categoryList>
-  <fileList :list="topFileList" v-else="tabIndex !== 0"></fileList>
-</transition>
-   </section>
+      <h3 class="title text_shdow">这里有你想要的所有</h3>
+      <div id="search"
+           @click="$router.push('/search')">
+        <img src="../assets/search.png"
+             class="img vertical_center">
+        <span>搜索...</span>
+      </div>
+      <div class="upload text_shdow"
+           @click="$router.push('/upload')">
+        <img class="img vertical_center"
+             src="../assets/upload.png">
+        <span>上传文件</span>
+      </div>
+    </header>
+    <section class="slide_show">
+      <swiper loop
+              :list="imgList"
+              auto
+              :show-desc-mask="false"
+              style="width:100%;margin:0 auto; height:100px;"
+              :aspect-ratio="100/375"
+              dots-class="custom-bottom"
+              dots-position="center"></swiper>
+    </section>
+    <section class="list">
+      <Tab v-model="tabIndex"
+           :active-color="mainColor"
+           :bar-active-color="mainColor"
+           :line-width="2"
+           defaultColor="#00B9F8">
+        <tab-item :selected="item === currentTab"
+                  v-for="(item, index) in tabList"
+                  :key="index">{{item}}</tab-item>
+      </Tab>
+      <transition name="fade">
+        <categoryList v-if="tabIndex === 0"
+                      @click="categoryListClick"></categoryList>
+        <fileList :list="topFileList"
+                  v-else="tabIndex !== 0"></fileList>
+      </transition>
+    </section>
   </div>
 </template>
 <script>
-import { Swiper, Tab, TabItem, SwiperItem} from 'vux'
-import {categoryList, fileList} from '../components/'
+import { Swiper, Tab, TabItem, SwiperItem } from 'vux'
+import { categoryList, fileList } from '../components/'
 import { mapState } from 'vuex'
 
-// const imgList = [
-//   'http://placeholder.qiniudn.com/100x100/FF3B3B/ffffff',
-//   'http://placeholder.qiniudn.com/800x100/FFEF7D/ffffff',
-//   'http://placeholder.qiniudn.com/800x100/8AEEB1/ffffff'
-// ]
-// const demoList = imgList.map((one, index) => ({
-//   url: 'javascript:',
-//   img: one
-// }))
+const defaultImageList = [{
+        img: 'http://placeholder.qiniudn.com/200x750/FF3B3B/ffffff',
+        url: 'http://www.baidu.com'
+      },{
+        img: 'http://placeholder.qiniudn.com/100x375/FF3B3B/ffffff',
+        url: 'http://www.baidu.com'
+      },{
+         img: 'http://placeholder.qiniudn.com/300x1125/FF3B3B/ffffff',
+        url: 'http://www.baidu.com'
+      }
+      ]
 
 const tabList = ['全部分类', '本校热门']
 export default {
@@ -69,18 +86,28 @@ export default {
     SwiperItem,
     fileList
   },
-  mounted () {
+  mounted() {
     this.$store.dispatch('initUserInfo')
     this.$http.get('/index/home').then(data => {
-    const body = data.body
-    this.imgList = body.banner
-    this.topFileList = body['top_file']
+      const body = data.body
+      if (body.banner) {
+
+      this.imgList = body.banner.map(i => {
+        return {
+          img: i.image,
+          url: i.url
+        }
+      })
+      } else {
+        this.imgList = defaultImageList
+      }
+      this.topFileList = body['top_file']
     })
   },
-  data () {
+  data() {
     return {
       tabIndex: 0,
-      currentTab:tabList[0],
+      currentTab: tabList[0],
       mainColor: "#F8421E",
       imgList: [],
       tabList: tabList,
@@ -90,7 +117,7 @@ export default {
   },
   methods: {
     categoryListClick(val) {
-    	this.$router.push({path: '/search', query: { type: val.id }})
+      this.$router.push({ path: '/search', query: { type: val.id } })
     }
   },
   computed: {
@@ -104,25 +131,25 @@ export default {
 <style lang="scss" scoped>
 @import '../style/base';
 @import '../style/animate';
-#basic_info{
+#basic_info {
   padding: 0 28px;
   display: flex;
   align-items: center;
   justify-content: space-between;
-  .avatar_photo{
-    width:28px;
+  .avatar_photo {
+    width: 28px;
     height: 28px;
     border-radius: 100%;
   }
-  .right{
+  .right {
     .img {
-      width:10px;
+      width: 10px;
       height: 16px;
     }
   }
 }
 
-#header{
+#header {
   text-align: center;
   color: white;
   display: flex;
@@ -130,39 +157,40 @@ export default {
   justify-content: space-around;
   height: 205px;
   background-image: linear-gradient(-180deg, #00B9F8 0%, #2ECAFF 100%);
-  .title{
+  .title {
     /* 这有你想要的所有: */
-font-weight: 300;
-font-size: 20px;
-color: #FFFFFF;
-letter-spacing: -0.49px;
+    font-weight: 300;
+    font-size: 20px;
+    color: #FFFFFF;
+    letter-spacing: -0.49px;
   }
-  .upload{
+  .upload {
     padding: 8px;
-    .img{
+    .img {
       width: 15px;
       height: 15px;
     }
   }
 }
+
 #search {
   box-sizing: content-box;
   text-align: left;
-  margin: 0 28px;
-  // line-height: 48px;
+  margin: 0 28px; // line-height: 48px;
   padding: 12px 17px;
   color: #9E9E9E;
-  line-height:22px;
-  .img{
+  line-height: 22px;
+  .img {
     padding-right: 5px;
     width: 21px;
     height: 21px;
   }
-background: #FFFFFF;
-box-shadow: 0px 1px 4px 0px rgba(0,0,0,0.18);
-border-radius: 25px;
+  background: #FFFFFF;
+  box-shadow: 0px 1px 4px 0px rgba(0, 0, 0, 0.18);
+  border-radius: 25px;
 }
-.slide_show{
-  height:100px;
+
+.slide_show {
+  // height: 100px;
 }
 </style>
