c20d40df5a3b05f74edff5094f057c60ae8e05e3
broven <zycode277@163.com>
2017-04-19 16:45:55 +0800

    对category列表进行顺序调整

--- front-end/src/utils.js
+++ front-end/src/utils.js
@@ -1,5 +1,5 @@
-export const ids =        [2        ,4        ,8      ,16    ,32       ,64         ,128,256]
-export const titleList = ['科目精华', '科目习题', '英语', '考研', '资格考试', '课外资料', '其他','病例']
+export const ids =        [2        ,4        ,8      ,16    ,32       ,64         ,256,  128  ]
+export const titleList = ['科目精华', '科目习题', '英语', '考研', '资格考试', '课外资料','病例', '其他']
 export function getCategroyListById(id) {
   id = Number(id)
   return titleList[ids.indexOf(id)]
