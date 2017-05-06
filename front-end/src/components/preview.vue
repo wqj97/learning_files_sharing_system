1a6df0c254d81facedf7b1e01e48bd693d5a54ea
meta <zycode277@163.com>
2017-05-06 19:28:23 +0800

    update

--- front-end/src/components/preview.vue
+++ front-end/src/components/preview.vue
@@ -2,7 +2,7 @@
   <div>
   <div class="slogan" v-if="arr.length === 0">该资料目前不支持预览, 请下载后查看</div>
   <img class="preview" v-for="i in arr" :src="i">
-  <div class="slogan bottom" v-show="arr.length !== 0">更多内容,请下载后查看</div>
+  <div class="slogan bottom" v-show="arr.length !== 0 && arr.length < 5">更多内容,请下载后查看</div>
   </div>
 </template>
 
