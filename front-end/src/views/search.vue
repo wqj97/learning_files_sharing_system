cb6ecc4b0a5408595062e902c6602e46f8a3e578
meta <zycode277@163.com>
2017-05-27 19:35:29 +0800

    搜索路由添加

--- front-end/src/views/search.vue
+++ front-end/src/views/search.vue
@@ -1,25 +1,10 @@
 <template>
   <div>
-    <searchBar
-               v-if="passIn"
-               shadow
-               :type="type[0]"
-               v-model="search"
-               @submit="submit"></searchBar>
-     <searchBar
-               v-else
-               shadow
-               v-model="search"
-               @submit="submit"></searchBar>
+    <searchBar v-if="passIn" shadow :type="type[0]" v-model="search" @submit="submit"></searchBar>
+    <searchBar v-else shadow v-model="search" @submit="submit"></searchBar>
     <main class="container">
-      <fileList isScroll
-                :list="searchResult"
-                v-if="isSearch || passIn"
-                @refresh="loadMore"
-                ref="scroll"></fileList>
-      <categoryList v-show="!passIn && !isSearch"
-                    v-model="type"
-                    checkBox></categoryList>
+      <fileList isScroll :list="searchResult" v-if="isSearch || passIn" @refresh="loadMore" ref="scroll"></fileList>
+      <categoryList v-show="!passIn && !isSearch" v-model="type" checkBox></categoryList>
     </main>
   </div>
 </template>
@@ -33,11 +18,17 @@ export default {
   },
   created() {
     let id = this.$route.query.type
+    let searchData = this.$route.query.search
     if (id) {
-      this.type=[]
+      this.type = []
       this.type.push(Number(id))
       this.passIn = true
-      this.searchData()
+      if (searchData == undefined || searchData === '') {
+        this.searchData()
+      } else {
+        this.search = searchData
+        this.submit()
+      }
     } else {
       this.type.push(0)
     }
@@ -51,18 +42,19 @@ export default {
       this.isSearch = true         // 显示list组件
       this.searchResult = []
       this.$nextTick(function () {
-    this.$refs.scroll.resetState()
-    })
+        this.$refs.scroll.resetState()
+      })
       this.searchData()
+      this.$router.replace({ path: this.$route.fullPath, query: { search: this.search } })
     },
     searchData() {
-       this.$store.commit('updateLoadingStatus', {isLoading: true})
+      this.$store.commit('updateLoadingStatus', { isLoading: true })
       this.$http.get(`/search?name=${this.search}&page=${this.page}&type=${JSON.stringify(this.type)}`).then(res => {
-        this.$store.commit('updateLoadingStatus', {isLoading: false})
+        this.$store.commit('updateLoadingStatus', { isLoading: false })
         if (res.body.length === 0) {
           this.$refs.scroll.noMoreData()
           return
-      }
+        }
         this.searchResult = this.searchResult.concat(res.body)
         this.$refs.scroll.done()
       }, res => {
