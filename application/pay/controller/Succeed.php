47ece15d1a9c943d51eb4a4aeb654c2b86c0dec8
Wanqj <wqj97@126.com>
2017-06-12 00:43:33 +0800

    功能完善

--- application/pay/controller/Succeed.php
+++ application/pay/controller/Succeed.php
@@ -10,6 +10,7 @@ namespace app\pay\controller;
 
 use think\Db;
 use think\Debug;
+use think\Log;
 
 class Succeed
 {
@@ -23,25 +24,18 @@ class Succeed
         $level = json_decode(Server_Setting('level'), true);
         $price = json_decode(Server_Setting('price'), true);
         $credit = 0;
-        echo $amount."\r\n";
         foreach ($price as $key => $val) {
             if ($val - $amount >= 0) {
-                if ($val - $amount == 0) {
-                    $credit = $price[0];
-                }
-                else {
-                    if ($amount < $price[0]) {
-                        $credit = 0;
-                    } else {
-                        $credit = $level[$key - 1];
-                    }
-                }
+                $credit = $level[$key + 1];
+                break;
             }
         }
         if ($amount > $price[count($price) - 1]){
             $credit = $level[count($level) - 1];
         }
-        echo $credit;
+        if ($amount < $price[0]) {
+            $credit = 0;
+        }
         $O_openid = Db::query("SELECT O_openid FROM Pay_Order WHERE O_Id = ?", [$order_no])[0]['O_openid'];
         Db::execute('UPDATE Pay_Order SET O_state = 2 WHERE O_Id = ?', [$order_no]);
         Db::execute('UPDATE `User` SET `U_credit` = `U_credit` + ?  WHERE U_openid = ?', [$credit, $O_openid]);
