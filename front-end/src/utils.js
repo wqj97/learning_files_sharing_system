13fb9ccdb0e9db96371b0688314e7cd2e96d6801
Wanqj <wqj97@126.com>
2017-11-19 13:48:05 +0800

    甲方

--- front-end/src/utils.js
+++ front-end/src/utils.js
@@ -1,5 +1,5 @@
 export const ids =        [2        ,4        ,8      ,16    ,32       ,64         ,128,  256  ]
-export const titleList = ['科目精华', '科目习题', '英语', '考研', '资格考试', '课外资料','病例', '其他']
+export const titleList = ['中医', '西医', '护理', '药学', '中西医结合', '课外资料','病例', '其他']
 export function getCategroyListById(id) {
   id = Number(id)
   return titleList[ids.indexOf(id)]
--- front-end/src/views/index.vue
+++ front-end/src/views/index.vue
@@ -3,17 +3,18 @@
     <!--<div class="wrapper">
     <iframe :src="agreementUrl" frameborder="0"></iframe>
   </div>-->
-  <x-dialog  :scroll="false" v-model="isShowDialog"
-  >
-  <div>用户协议</div>
+    <x-dialog :scroll="false" v-model="isShowDialog"
+    >
+      <div>用户协议</div>
 
-  <div class="wrapper">
-    <iframe style="height:100%;overflow:scroll;" :src="agreementUrl" frameborder="0"></iframe>
-  </div>
-  <div class="oper">
-  <x-button type="primary" mini @click.native="agree">同意</x-button> <x-button mini @click.native="reject">拒绝</x-button>
-  </div>
-  </x-dialog>
+      <div class="wrapper">
+        <iframe style="height:100%;overflow:scroll;" :src="agreementUrl" frameborder="0"></iframe>
+      </div>
+      <div class="oper">
+        <x-button type="primary" mini @click.native="agree">同意</x-button>
+        <x-button mini @click.native="reject">拒绝</x-button>
+      </div>
+    </x-dialog>
     <header id="header">
       <div id="basic_info"
            class="text_shdow">
@@ -61,7 +62,8 @@
            defaultColor="#00B9F8">
         <tab-item :selected="item === currentTab"
                   v-for="(item, index) in tabList"
-                  :key="index">{{item}}</tab-item>
+                  :key="index">{{item}}
+        </tab-item>
       </Tab>
       <transition name="fade">
         <categoryList v-if="tabIndex === 0"
@@ -73,169 +75,175 @@
   </div>
 </template>
 <script>
-import { Swiper, Tab, TabItem, SwiperItem, XDialog , XButton} from 'vux'
-import { categoryList, fileList } from '../components/'
-import { mapState } from 'vuex'
-import {assetsPath} from '../utils'
-const defaultImageList = [{
-        img: 'http://placeholder.qiniudn.com/200x750/FF3B3B/ffffff',
-        url: 'http://www.baidu.com'
-      },{
-        img: 'http://placeholder.qiniudn.com/100x375/FF3B3B/ffffff',
-        url: 'http://www.baidu.com'
-      },{
-         img: 'http://placeholder.qiniudn.com/300x1125/FF3B3B/ffffff',
-        url: 'http://www.baidu.com'
-      }
-      ]
+  import {Swiper, Tab, TabItem, SwiperItem, XDialog, XButton} from 'vux'
+  import {categoryList, fileList} from '../components/'
+  import {mapState} from 'vuex'
+  import {assetsPath} from '../utils'
 
-const tabList = ['全部分类', '本校热门']
-export default {
-  name: '',
-  components: {
-    Swiper,
-    Tab,
-    TabItem,
-    categoryList,
-    SwiperItem,
-    fileList,
-    XDialog,
-    XButton
-  },
-  mounted() {
-    this.isShowDialog = localStorage.isAgree === "true" ? false : true
-    this.$store.dispatch('initUserInfo')
-    this.$http.get('/index/home').then(data => {
-      const body = data.body
-      if (body.banner) {
-
-      this.imgList = body.banner.map(i => {
-        return {
-          img: i.image,
-          url: i.url
-        }
-      })
-      } else {
-        this.imgList = defaultImageList
-      }
-      this.topFileList = body['top_file']
-    })
-  },
-  data() {
-    return {
-      isShowDialog: false,
-      tabIndex: 0,
-      currentTab: tabList[0],
-      mainColor: "#F8421E",
-      imgList: [],
-      tabList: tabList,
-      topFileList: []
-      // university: localStorage.schoolName
-    }
-  },
-  methods: {
-    agree() {
-      localStorage.isAgree=true
-      this.isShowDialog=false
-    },
-    reject() {
-      localStorage.isAgree=false
-      alert('感谢使用! 请退出')
-    },
-    categoryListClick(val) {
-      this.$router.push({ path: '/search', query: { type: val.id } })
-    }
-  },
-  computed: {
-    agreementUrl() {
-      return assetsPath + 'img/agreement.html'
-    },
-    ...mapState({
-      user: state => state.user
-    })
+  const defaultImageList = [{
+    img: 'http://placeholder.qiniudn.com/200x750/FF3B3B/ffffff',
+    url: 'http://www.baidu.com'
+  }, {
+    img: 'http://placeholder.qiniudn.com/100x375/FF3B3B/ffffff',
+    url: 'http://www.baidu.com'
+  }, {
+    img: 'http://placeholder.qiniudn.com/300x1125/FF3B3B/ffffff',
+    url: 'http://www.baidu.com'
   }
-}
+  ]
+
+  const tabList = ['全部分类', '本校热门']
+  export default {
+    name: '',
+    components: {
+      Swiper,
+      Tab,
+      TabItem,
+      categoryList,
+      SwiperItem,
+      fileList,
+      XDialog,
+      XButton
+    },
+    mounted () {
+      this.isShowDialog = localStorage.isAgree === "true" ? false : true
+      this.$store.dispatch('initUserInfo')
+      this.$http.get('/index/home').then(data => {
+        const body = data.body
+        if (body.banner) {
+
+          this.imgList = body.banner.map(i => {
+            return {
+              img: i.image,
+              url: i.url
+            }
+          })
+        } else {
+          this.imgList = defaultImageList
+        }
+        this.topFileList = body['top_file']
+      })
+    },
+    data () {
+      return {
+        isShowDialog: false,
+        tabIndex: 0,
+        currentTab: tabList[0],
+        mainColor: "#F8421E",
+        imgList: [],
+        tabList: tabList,
+        topFileList: []
+        // university: localStorage.schoolName
+      }
+    },
+    methods: {
+      agree () {
+        localStorage.isAgree = true
+        this.isShowDialog = false
+      },
+      reject () {
+        localStorage.isAgree = false
+        alert('感谢使用! 请退出')
+      },
+      categoryListClick (val) {
+        this.$router.push({path: '/search', query: {type: val.id}})
+      }
+    },
+    computed: {
+      agreementUrl () {
+        return assetsPath + 'img/agreement.html'
+      },
+      ...mapState({
+        user: state => state.user
+      })
+    }
+  }
 </script>
 
-<style lang="scss" scoped>
-@import '../style/base';
-@import '../style/animate';
-#basic_info {
-  padding: 0 28px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  .avatar_photo {
-box-shadow: 0px 2px 7px 0px rgba(0,0,0,0.24);
-    width: 28px;
-    height: 28px;
-    border-radius: 100%;
+<style type="text/scss" lang="scss" scoped>
+  @import '../style/base';
+  @import '../style/animate';
+
+  #basic_info {
+    padding: 0 28px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .avatar_photo {
+      box-shadow: 0px 2px 7px 0px rgba(0, 0, 0, 0.24);
+      width: 28px;
+      height: 28px;
+      border-radius: 100%;
+    }
+    .right {
+      .img {
+        width: 10px;
+        height: 16px;
+      }
+    }
   }
-  .right {
+
+  #header {
+    text-align: center;
+    color: white;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    height: 205px;
+    background-image: linear-gradient(-180deg, #00B9F8 0%, #2ECAFF 100%);
+    .title {
+      /* 这有你想要的所有: */
+      font-weight: 300;
+      font-size: 20px;
+      color: #FFFFFF;
+      letter-spacing: -0.49px;
+    }
+    .upload {
+      padding: 8px;
+      .img {
+        width: 15px;
+        height: 15px;
+      }
+      span {
+        text-shadow: 0px 2px 12px rgba(0, 0, 0, 0.7);
+      }
+    }
+  }
+
+  #search {
+    box-sizing: content-box;
+    text-align: left;
+    margin: 0 28px; // line-height: 48px;
+    padding: 12px 17px;
+    color: #bbbbbb;
+    line-height: 22px;
     .img {
-      width: 10px;
-      height: 16px;
+      padding-right: 5px;
+      width: 21px;
+      height: 21px;
     }
+    background: #FFFFFF;
+    box-shadow: 0px 1px 4px 0px rgba(0, 0, 0, 0.18);
+    border-radius: 25px;
   }
-}
 
-#header {
-  text-align: center;
-  color: white;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  height: 205px;
-  background-image: linear-gradient(-180deg, #00B9F8 0%, #2ECAFF 100%);
-  .title {
-    /* 这有你想要的所有: */
-    font-weight: 300;
-    font-size: 20px;
-    color: #FFFFFF;
-    letter-spacing: -0.49px;
+  .slide_show {
+    // height: 100px;
   }
-  .upload {
-    padding: 8px;
-    .img {
-      width: 15px;
-      height: 15px;
-    }
-    span{
-      text-shadow: 0px 2px 12px rgba(0, 0, 0, 0.7);
-    }
-  }
-}
 
-#search {
-  box-sizing: content-box;
-  text-align: left;
-  margin: 0 28px; // line-height: 48px;
-  padding: 12px 17px;
-  color: #bbbbbb;
-  line-height: 22px;
-  .img {
-    padding-right: 5px;
-    width: 21px;
-    height: 21px;
-  }
-  background: #FFFFFF;
-  box-shadow: 0px 1px 4px 0px rgba(0, 0, 0, 0.18);
-  border-radius: 25px;
-}
-
-.slide_show {
-  // height: 100px;
-}
-.wrapper{
-    height:40vh;
+  .wrapper {
+    height: 40vh;
     overflow: hidden;
-    -webkit-overflow-scrolling:touch; overflow: scroll;
+    -webkit-overflow-scrolling: touch;
+    overflow: scroll;
   }
-.oper{
-  padding: 10px;
 
-}
-.weui-btn + .weui-btn{
-  margin-top: 0px;
-}
+  .oper {
+    padding: 10px;
+
+  }
+
+  .weui-btn + .weui-btn {
+    margin-top: 0px;
+  }
 </style>
