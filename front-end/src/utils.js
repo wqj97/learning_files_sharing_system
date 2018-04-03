ca4409653f1390e759bba0ea4177bbe101a949a0
Wanqj <wqj97@126.com>
2018-04-03 16:52:54 +0800

    2018-04-03 维护

--- front-end/src/utils.js
+++ front-end/src/utils.js
@@ -1,12 +1,14 @@
-export const ids =        [2        ,4        ,8      ,16    ,32       ,64         ,128,  256  ]
-export const titleList = ['中医', '西医', '护理', '药学', '中西医结合', '课外资料','病例', '其他']
-export function getCategroyListById(id) {
+export const ids = [2, 4, 8, 16, 32, 64, 128, 256]
+export const titleList = ['中医精华', '中医习题', '西医精华', '西医习题', '护理精华', '护理习题', '临床医案', '课外书籍']
+
+export function getCategroyListById (id) {
   id = Number(id)
   return titleList[ids.indexOf(id)]
 }
-export const assetsPath = process.env.NODE_ENV === 'development' ?  '/static/' : '/home/static/'
 
-export function isComputer() {
+export const assetsPath = process.env.NODE_ENV === 'development' ? '/static/' : '/home/static/'
+
+export function isComputer () {
   let platform = window.navigator.platform
   console.log(platform)
   return /(mac|win|x11)/.test(platform.toLowerCase())
